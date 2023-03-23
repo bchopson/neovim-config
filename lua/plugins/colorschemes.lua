1c51688d1ffc8d96c13ecb9d255def5bdf30da22
Ben Chopson <bchopson@users.noreply.github.com>
2023-03-23 13:10:11 -0400

    Updates, colorscheme, fugitive

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,9 +1,10 @@
 return {
   { "rebelot/kanagawa.nvim" },
+  { "savq/melange-nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "melange",
     },
   },
 }
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
   {
     "tpope/vim-fugitive",
+    lazy = false,
     keys = {
       { "<leader>gl", ":0Gclog<CR>", desc = "0Gclog" },
       { "<leader>gd", ":Gdiff<CR>", desc = "Gdiff" },
