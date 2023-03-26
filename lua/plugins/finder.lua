ab5d10ff84328decd9096a65902c7d5852a1d76a
Ben Chopson <bchopson@users.noreply.github.com>
2023-03-25 22:30:23 -0400

    Enable rg args for telescope

---
+++ lua/plugins/finder.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "nvim-telescope/telescope-live-grep-args.nvim" },
+  },
+  config = function()
+    require("telescope").load_extension("live_grep_args")
+  end,
+  keys = {
+    { "<leader>/", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>" },
+  },
+}
