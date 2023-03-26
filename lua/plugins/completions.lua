ecd2707ef08f08c33550c615df36101a2a9e34b6
Ben Chopson <bchopson@users.noreply.github.com>
2023-03-26 10:15:11 -0400

    Configure completions

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<CR>"] = cmp.mapping({
+          i = function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            else
+              fallback()
+            end
+          end,
+          s = cmp.mapping.confirm({ select = true }),
+          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
+      })
+    end,
+  },
+}
