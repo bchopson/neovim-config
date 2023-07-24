0067b3601f2a4039651060e22a3cf4747643586c
Ben Chopson <bchopson@users.noreply.github.com>
2023-07-24 13:24:44 -0400

    Revert to telescope for live grep

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -1,26 +1,14 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    -- dependencies = {
-    --   { "nvim-telescope/telescope-live-grep-args.nvim" },
-    -- },
-    -- config = function()
-    --   require("telescope").load_extension("live_grep_args")
-    -- end,
-    -- keys = {
-    --   { "<leader>/", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>" },
-    -- },
-    keys = {
-      { "<leader>/", vim.NIL },
-    },
-  },
-  {
-    "junegunn/fzf.vim",
     dependencies = {
-      { "junegunn/fzf" },
+      { "nvim-telescope/telescope-live-grep-args.nvim" },
     },
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end,
     keys = {
-      { "<leader>/", "<cmd>Rg<CR>" },
+      { "<leader>/", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>" },
     },
   },
 }
