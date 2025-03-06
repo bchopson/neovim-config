6d332d5f2b24d80b3ae5b76fe9cb364db69e99b7
Ben Chopson <bchopson@users.noreply.github.com>
2025-03-06 15:39:11 -0500

    Try Augment

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,16 @@
+require("which-key").add({
+  { "<leader>z", group = "ai" },
+})
 return {
   { "madox2/vim-ai" },
+  {
+    "augmentcode/augment.vim",
+    lazy = false,
+    keys = {
+      { "<leader>zc", ":Augment chat<CR>", desc = "Augment Chat" },
+      { "<leader>zz", ":Augment chat-toggle<CR>", desc = "Toggle Augment Chat" },
+    },
+  },
   {
     "echasnovski/mini.bracketed",
     main = "mini.bracketed",
